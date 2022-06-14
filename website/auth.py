89d22d51f01623653d9abecf5c5519b807b41c32
wgoh26 <wgoh26@ssis.edu.vn>
2022-06-14 18:03:49 +0700

    second commit

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,25 @@
-from crypt import methods
-from turtle import home
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully.', category='success')
+            else:
+                flash('Incorrect password.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -14,4 +28,27 @@ def logout():
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('An account with that email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Your email has to longer than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('Your first name has to longer than 1 character.', category='error')
+        elif len(password1) < 8:
+            flash('Your password has to be 8 characters or more.', category='error')
+        elif password1 != password2:
+            flash('Your passwords do not match.', category='error')
+        else:
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit
+            flash('Your account has been created!', category='success')
+            return redirect(url_for('views.home'))
     return render_template("signup.html")
